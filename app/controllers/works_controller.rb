20e4a1a48a3ec11dc3ffd9f56e1f4bcb6b2dea99
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-11-11 15:57:05 -0800

    homepage initiate only the rest in separte branch

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,11 +1,18 @@
 class WorksController < ApplicationController
+  def homepage
+  end
+
+
   def index
+    # @works = Work.all
     @albums = Work.albums
     @books = Work.books
     @movies = Work.movies
   end
 
   def show
+    id = params[:id]
+    @work = Work.find_by(id: id)
   end
 
   def new
@@ -13,4 +20,12 @@ class WorksController < ApplicationController
 
   def edit
   end
+
+  private
+
+  # def work_params
+  #   return params.require(:work).permit(:title, :description, :publication_year, :creator, :category)
+  # end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: 'works#homepage'
+
   get 'votes/index'
   get 'votes/show'
   get 'votes/new'
