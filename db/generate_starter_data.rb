dac583f6e51ca2a3a5e903ee0677879ccfb1586f
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-11-10 20:27:08 -0800

    generated starter data

--- db/generate_starter_data.rb
+++ db/generate_starter_data.rb
@@ -13,12 +13,33 @@ require "csv"
 CSV.open("db/works_seeds.csv", "w", :write_headers => true,
                                     :headers => ["category", "title", "creator", "publication_year", "description"]) do |csv|
   25.times do
-    category = %w(album book).sample
+    category = %w(album book movie).sample
     title = Faker::Coffee.blend_name
     creator = Faker::Name.name
     publication_year = rand(Date.today.year - 100..Date.today.year)
     description = Faker::Lorem.sentence
 
     csv << [category, title, creator, publication_year, description]
+  end
+end
+
+CSV.open("db/users_seeds.csv", "w", :write_headers => true,
+         :headers => ["name", "date_joined"]) do |csv|
+  25.times do
+    name = Faker::Name.name
+    date_joined = rand((Date.today - 100)..(Date.today - 50))
+
+    csv << [name, date_joined]
+  end
+end
+
+CSV.open("db/votes_seeds.csv", "w", :write_headers => true,
+         :headers => ["user_id", "work_id", "date voted"]) do |csv|
+  40.times do
+    user_id = rand(1..25)
+    work_id = rand(1..25)
+    date_voted = rand((Date.today - 49)..(Date.today))
+
+    csv << [user_id, work_id, date_voted]
   end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,81 @@
+require 'csv'
+
+WORK_FILE = Rails.root.join('db', 'seed_data', 'works-seeds.csv')
+puts "Loading raw work data from #{WORK_FILE}"
+
+work_failures = []
+CSV.foreach(WORK_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+  successful = work.save
+  if !successful
+    work_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
+
+
+
+USER_FILE = Rails.root.join('db', 'seed_data', 'users.csv')
+puts "Loading raw user data from #{USER_FILE}"
+
+user_failures = []
+CSV.foreach(USER_FILE, :headers => true) do |row|
+  user = User.new
+  user.name = row['name']
+  user.date_joined = row['date_joined']
+  successful = user.save
+  if !successful
+    user_failures << user
+    puts "Failed to save user: #{user.inspect}"
+  else
+    puts "Created user: #{user.inspect}"
+  end
+end
+
+puts "Added #{User.count} user records"
+puts "#{user_failures.length} users failed to save"
+
+
+
+VOTE_FILE = Rails.root.join('db', 'seed_data', 'votes.csv')
+puts "Loading raw vote data from #{VOTE_FILE}"
+
+vote_failures = []
+CSV.foreach(VOTE_FILE, :headers => true) do |row|
+  vote = Vote.new
+  vote.work_id = row['work_id']
+  vote.user_id = row['user_id']
+  vote.voted_on = Date.strptime(row['date'], '%Y-%m-%d')
+  successful = vote.save
+  if !successful
+    vote_failures << vote
+    puts "Failed to save vote: #{vote.inspect}"
+  else
+    puts "Created vote: #{vote.inspect}"
+  end
+end
+
+puts "Added #{Vote.count} vote records"
+puts "#{vote_failures.length} votes failed to save"
+
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+puts "done"
