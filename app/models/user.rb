b0bb7d06ab026ca2f7b12979338624da05f618a0
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-11-16 00:23:29 -0800

    minor changes to user and vote models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,20 @@ class User < ApplicationRecord
   has_many :works, through: :votes
 
   validates :name, presence: { message: "• name: can't be blank" }
-  # , uniqueness: { message: "• must be unique" }
 
   def self.vote_count(user)
-    unless user.nil?
+    if user.nil? 
+      return nil
+    else
       return user.votes.count
     end
   end
 
   def self.date_joined(user)
     unless user.nil?
-      return(user.created_at.strftime("%b %d, %Y"))
+      return user.created_at.strftime("%b %d, %Y")
+    else
+      return nil
     end
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,9 +3,6 @@ class Vote < ApplicationRecord
   belongs_to :user
 
   validates :work_id, uniqueness: { scope: :user_id, message: "• user: has already voted for this work" }
-
-
-  # message: "User cannot vote twice on same work" }
 end
 
 
